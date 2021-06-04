8960e4c62035bca40bbaadd8589e1f5c8dd8ccb8
grewer <grewer@grewer.cn>
2021-06-04 11:35:23 +0800

    feat: flooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import useModel from 'flooks'
+
+const counter = (now) => ({
+  count: 0,
+  test: 1,
+  add() {
+    const { count } = now() // <----- now()        :: 获取自身 model
+    now({ count: count + 1 }) // <--- now(payload) :: 更新自身 model
+  },
+  async addLater() {
+    const { add, test } = now() // <-- now(model) :: 获取其它 model
+    now({ test: test + 1 })
+    await new Promise((resolve, reject) => setTimeout(resolve, 1000))
+    add()
+  },
+})
 
 function App() {
+  const { count, add, addLater } = useModel(counter) // <-- ['count'] :: 按需触发 Re-render
+
+  console.log(addLater.loading, addLater.error)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{count}</p>
+      <button onClick={add}>+</button>
+      <button onClick={addLater}>+ ⌛{addLater.loading && '...'}</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
